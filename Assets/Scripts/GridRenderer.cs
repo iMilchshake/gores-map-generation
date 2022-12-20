069ace702d315b492863443c02e8df877c489e2e
Tobias Schneider <tobias.schneider@smail.inf.h-brs.de>
2022-12-20 22:36:48 +0100

    Add simple random walker

--- Assets/Scripts/GridRenderer.cs
+++ Assets/Scripts/GridRenderer.cs
@@ -1,33 +1,25 @@
+using System;
 using UnityEngine;
+using UnityEngine.Animations;
+using Object = UnityEngine.Object;
+using Random = System.Random;
 
 public class GridRenderer : MonoBehaviour
 {
     public GameObject squarePrefab;
     public GridDisplay GridDisplay;
 
-    private const int Width = 10;
-    private const int Height = 10;
-
-    private BlockType[,] _grid = new BlockType[Width, Height];
-
     void Start()
     {
-        for (int x = 0; x < Width; x++)
+        var mapGen = new MapGenerator(42, 100, 100);
+        for (int iteration = 0; iteration < 500; iteration++)
         {
-            for (int y = 0; y < Height; y++)
-            {
-                if ((x + y) % 5 == 0)
-                {
-                    _grid[x, y] = BlockType.Unhookable;
-                }
-                else
-                {
-                    _grid[x, y] = BlockType.Hookable;
-                }
-            }
+            mapGen.Step();
         }
 
-        GridDisplay = new GridDisplay(squarePrefab, _grid);
+        var grid = mapGen.Grid;
+
+        GridDisplay = new GridDisplay(squarePrefab, grid);
     }
 }
 
@@ -35,7 +27,8 @@ public enum BlockType
 {
     Hookable,
     Unhookable,
-    Freeze
+    Freeze,
+    Empty
 }
 
 
@@ -44,6 +37,7 @@ public class GridDisplay
     public Color Hookable = new Color(1f, 0.86f, 0.27f);
     public Color Unhookable = new Color(0.29f, 0.45f, 0.5f);
     public Color Freeze = new Color(0.01f, 0f, 0.02f);
+    public Color Empty = new Color(1.0f, 1.0f, 1.0f, 0.1f);
     private readonly GameObject _squarePrefab;
 
     public GridDisplay(GameObject squarePrefab, BlockType[,] grid)
@@ -60,22 +54,16 @@ public class GridDisplay
 
     private GridTile InitializeSquare(Vector2 position, BlockType type)
     {
-        var square = Object.Instantiate(_squarePrefab, position, Quaternion.identity);
+        var square = Object.Instantiate(_squarePrefab, new Vector3(position.x, position.y, 1.0f), Quaternion.identity);
         var render = square.GetComponent<SpriteRenderer>();
-
-        switch (type)
+        render.color = type switch
         {
-            case BlockType.Hookable:
-                render.color = Hookable;
-                break;
-            case BlockType.Unhookable:
-                render.color = Unhookable;
-                break;
-            case BlockType.Freeze:
-                render.color = Freeze;
-                break;
-        }
-
+            BlockType.Freeze => Freeze,
+            BlockType.Unhookable => Unhookable,
+            BlockType.Hookable => Hookable,
+            BlockType.Empty => Empty,
+            _ => Empty
+        };
 
         return new GridTile(square, render);
     }
@@ -90,5 +78,36 @@ public class GridTile
     {
         this.obj = obj;
         this.spriteRenderer = spriteRenderer;
+    }
+}
+
+public class MapGenerator
+{
+    private readonly int _height;
+    private readonly int _width;
+    public BlockType[,] Grid;
+    private int WalkerPosX;
+    private int WalkerPosY;
+
+    private Random _rand;
+
+    public MapGenerator(int seed, int width, int height)
+    {
+        _width = width;
+        _height = height;
+        Grid = new BlockType[width, height];
+        WalkerPosX = _width / 2;
+        WalkerPosY = _height / 2;
+        _rand = new Random(seed);
+    }
+
+    public void Step()
+    {
+        int moveX = _rand.Next(-1, 2);
+        int moveY = _rand.Next(-1, 2);
+        WalkerPosX += moveX;
+        WalkerPosY += moveY;
+        Debug.Log($"{WalkerPosX}, {WalkerPosY}");
+        Grid[WalkerPosX, WalkerPosY] = BlockType.Empty;
     }
 }
