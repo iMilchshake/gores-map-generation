728be4e387241c9b0fd65650d5f51d591552f16e
tobi <Tobias20.3@hotmail.de>
2022-12-23 21:45:00 +0100

    Add MoveProbabilities
    - utility class that handles the probabilities for the respective next move

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using UnityEditor.VersionControl;
 using UnityEngine;
 
 public class Map
@@ -27,29 +28,6 @@ public class Map
         set => grid[x, y] = value;
     }
 
-    public string GetDebugString()
-    {
-        int emptyCount = 0;
-        int hookCount = 0;
-
-        for (int x = 0; x < width; x++)
-        {
-            for (int y = 0; y < height; y++)
-            {
-                if (this[x, y] == BlockType.Empty)
-                {
-                    emptyCount++;
-                }
-                else if (this[x, y] == BlockType.Hookable)
-                {
-                    hookCount++;
-                }
-            }
-        }
-
-        return $"hook={hookCount}, empty={emptyCount}";
-    }
-
     public static bool CheckSameDimension(Map map1, Map map2)
     {
         return map1.height == map2.height && map1.width == map2.width;
@@ -61,9 +39,57 @@ public class Map
     }
 }
 
+public class MoveProbabilities
+{
+    private float[,] _probabilities;
+    private int size;
+
+    public MoveProbabilities(int size)
+    {
+        _probabilities = new float[size, size];
+        this.size = size;
+    }
+
+    public float this[int x, int y]
+    {
+        get => _probabilities[MoveIndexToArrayIndex(x), MoveIndexToArrayIndex(y)];
+        set => _probabilities[MoveIndexToArrayIndex(x), MoveIndexToArrayIndex(y)] = value;
+    }
+
+    public float this[Vector2Int vec]
+    {
+        get => this[vec.x, vec.y];
+        set => this[vec.x, vec.y] = value;
+    }
+
+    private int MoveIndexToArrayIndex(int x)
+    {
+        // since _probabilities is squared, this function works for x and y
+        int centerX = (size - 1) / 2;
+        return centerX + x;
+    }
+
+    public Vector2Int[] GetAllValidMoves()
+    {
+        Vector2Int[] validMoves = new Vector2Int[size * size];
+        int index = 0;
+        for (int x = 0; x < size; x++)
+        {
+            for (int y = 0; y < size; y++)
+            {
+                validMoves[index] = new Vector2Int(x - (size - 1) / 2, y - (size - 1) / 2);
+                index++;
+            }
+        }
+
+        return validMoves;
+    }
+}
+
 public class MapGenerator
 {
     public Vector2Int WalkerPos;
+    public Vector2Int WalkerTargetPos;
 
     public Map Map;
     private int _width;
@@ -81,6 +107,8 @@ public class MapGenerator
     public void Initialize()
     {
         WalkerPos = new Vector2Int(_width / 2, _height / 2);
+        WalkerTargetPos = new Vector2Int(95, _height / 2);
+
         Map = new Map(_width, _height);
     }
 
@@ -97,8 +125,16 @@ public class MapGenerator
 
     public void Step()
     {
+        MoveProbabilities probabilities = new MoveProbabilities(3);
+        var validMoves = probabilities.GetAllValidMoves();
+
+        foreach (var move in validMoves)
+        {
+            var prob = probabilities[move];
+            Debug.Log(prob);
+        }
+
         WalkerPos += _rndGen.GetRandomDirectionVector();
-        // Debug.Log($"{WalkerPos.x}, {WalkerPos.y}");
         Map[WalkerPos.x, WalkerPos.y] = BlockType.Empty;
     }
 }
